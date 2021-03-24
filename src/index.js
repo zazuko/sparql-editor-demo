688327404be385e753c0e69cee153ce6be7d8184
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-03-24 13:53:09 +0100

    lazy load dependencies

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import { html, render } from 'lit-html/lit-html.js'
 import { asyncAppend } from 'lit-html/directives/async-append.js'
 import '@rdfjs-elements/sparql-editor/sparql-editor.js'
 import { turtle } from '@tpluscode/rdf-string'
-import $rdf from 'rdf-ext'
-import SparqlClient from 'sparql-http-client'
 import { termToNTriples } from '@rdf-esm/to-ntriples'
 import query from './query.js'
 
-const client = new SparqlClient({
-  endpointUrl: 'https://query.wikidata.org/sparql'
-})
 const editor = document.getElementById('editor')
 const results = document.getElementById('results')
 
@@ -30,13 +25,20 @@ function renderTable (selectResults) {
 
 // for DESCRIBE and CONSTRUCT queries, StreamClient returns an RDF/JS quad stream
 async function renderGraph (stream) {
+  const $rdf = (await import('rdf-ext')).default
+
   const dataset = await $rdf.dataset().import(stream)
 
   render(html`<pre>${turtle`${dataset}</pre>`}`, results)
 }
 
-function getResults ({ detail: { query, value } }) {
+async function getResults ({ detail: { query, value } }) {
   render('Running query', results)
+
+  const SparqlClient = (await import('sparql-http-client')).default
+  const client = new SparqlClient({
+    endpointUrl: 'https://query.wikidata.org/sparql'
+  })
 
   // Based on the query kind, run the appropriate client method
   switch (query.queryType) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const { createDefaultConfig } = require('@open-wc/building-webpack')
+const Visualizer = require('webpack-visualizer-plugin')
 
 module.exports = merge(
   createDefaultConfig({
     input: path.resolve(__dirname, './index.html')
   }),
   {
+    plugins: [new Visualizer()],
     resolve: {
       extensions: ['.mjs', '.js', '.json'],
       alias: {
